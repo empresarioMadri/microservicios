32c7cfde4ed13361777cc2adcdb484a2ed63fd0d
David <correo@davidrobledo.es>
2019-11-27 21:12:04 +0100

    Añadido control de propiedades con component

--- src/main/java/curso/cas/microservicios/ProyectoVideoClub/ProyectoVideoClubApplication.java
+++ src/main/java/curso/cas/microservicios/ProyectoVideoClub/ProyectoVideoClubApplication.java
@@ -6,14 +6,18 @@ import javax.servlet.http.HttpServlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import curso.cas.microservicios.ProyectoVideoClub.controller.WebController;
 import curso.cas.microservicios.ProyectoVideoClub.servlet.HolaServlet;
@@ -29,13 +33,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @Configuration
-public class ProyectoVideoClubApplication {
-	
+public class ProyectoVideoClubApplication implements CommandLineRunner {
+
 	private static final Logger log = LoggerFactory.getLogger(ProyectoVideoClubApplication.class);
-	
+
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ProyectoVideoClubApplication.class);
-		//app.setBannerMode(Banner.Mode.CONSOLE);
+		// app.setBannerMode(Banner.Mode.CONSOLE);
 		app.run(args);
 	}
 
@@ -52,14 +56,59 @@ public class ProyectoVideoClubApplication {
 				.license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0")
 				.build();
 	}
-	
+
 	@Bean
-	public ServletRegistrationBean<HttpServlet> holaServlet(){
+	public ServletRegistrationBean<HttpServlet> holaServlet() {
 		ServletRegistrationBean<HttpServlet> retorno = new ServletRegistrationBean<HttpServlet>();
 		retorno.setServlet(new HolaServlet());
 		retorno.addUrlMappings("/hola/*");
 		retorno.setLoadOnStartup(1);
 		return retorno;
+	}
+
+	@Autowired
+	MiAppProperties props;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		log.info("Version aplicacion " + props.getVersion());
+		log.info("Nombre aplicacion " + props.getNombre());
+		log.info("Servidor aplicacion " + props.getServidor());
+		
+	}
+
+	@Component
+	@ConfigurationProperties(prefix = "miapp")
+	public static class MiAppProperties {
+		private String servidor;
+		private String nombre;
+		private String version;
+
+		public String getServidor() {
+			return servidor;
+		}
+
+		public void setServidor(String servidor) {
+			this.servidor = servidor;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getVersion() {
+			return version;
+		}
+
+		public void setVersion(String version) {
+			this.version = version;
+		}
+
 	}
 
 }
