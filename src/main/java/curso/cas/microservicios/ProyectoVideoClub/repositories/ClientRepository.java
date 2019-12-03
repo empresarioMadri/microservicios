a9db70139fe75434c3a303dc8ac406aef9d09cc3
David <correo@davidrobledo.es>
2019-12-03 20:25:44 +0100

    Mas posibilidades de creación de queries con nombres

--- src/main/java/curso/cas/microservicios/ProyectoVideoClub/repositories/ClientRepository.java
+++ src/main/java/curso/cas/microservicios/ProyectoVideoClub/repositories/ClientRepository.java
@@ -7,11 +7,17 @@ import org.springframework.data.repository.CrudRepository;
 
 import curso.cas.microservicios.ProyectoVideoClub.entities.Cliente;
 
-public interface ClientRepository extends CrudRepository<Cliente, Integer>{
+public interface ClientRepository extends CrudRepository<Cliente, Integer> {
 
-	
 	Optional<Cliente> findById(Integer id);
-	
-	List<Cliente> findByNameOrEmail(String name,String email);
 
+	List<Cliente> findByNameOrEmail(String name, String email);
+
+	List<Cliente> findDistincClienteByNameOrEmail(String name, String email);
+	
+	List<Cliente> findByNameOrEmailIgnoreCase(String name, String email);
+	
+	List<Cliente> findByNameOrEmailOrderByNameAsc(String name, String email);
+	
+	List<Cliente> findByNameOrEmailOrderByNameDesc(String name, String email);
 }
