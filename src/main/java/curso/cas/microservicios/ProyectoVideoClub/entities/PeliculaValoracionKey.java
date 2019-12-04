12e0aa21d401e60ebc12bf03927bc3d1eb0c4113
David <correo@davidrobledo.es>
2019-12-04 20:28:33 +0100

    insertar registro con relacion manytomany

--- src/main/java/curso/cas/microservicios/ProyectoVideoClub/entities/PeliculaValoracionKey.java
+++ src/main/java/curso/cas/microservicios/ProyectoVideoClub/entities/PeliculaValoracionKey.java
@@ -1,11 +1,23 @@
 package curso.cas.microservicios.ProyectoVideoClub.entities;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+@RequiredArgsConstructor
+@ToString
 public class PeliculaValoracionKey implements Serializable{
 	
 	@Column(name="pelicula_id")
---
+++ src/main/java/curso/cas/microservicios/ProyectoVideoClub/repositories/PeliculaValoracionRepository.java
@@ -0,0 +1,9 @@
+package curso.cas.microservicios.ProyectoVideoClub.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import curso.cas.microservicios.ProyectoVideoClub.entities.PeliculaValoracion;
+
+public interface PeliculaValoracionRepository extends CrudRepository<PeliculaValoracion, Integer>{
+
+}
